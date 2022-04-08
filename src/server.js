013c81d46775818c8daadc6b40d49e32d5adcc37
rickyurvina <ricky_uc12@hotmail.com>
2022-04-08 16:28:50 -0500

    sdf

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/cloud-computing'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/cloud-computing/index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/cloud-computing'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/cloud-computing/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
